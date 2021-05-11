c64553b089b87aff5d38a106d33e737ddafb019b
unknown <milosacimo@gmail.com>
2021-05-12 00:00:39 +0200

    Finished

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const results = [];
+var total = 0;
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
   path: 'new_data.csv',
   header: [
-      {id: 'category', title: 'Category'},
-      {id: 'sales', title: 'Sales'}
+      {id: 'Category', title: 'Category'},
+      {id: 'Sales', title: 'Sales'},
+      {id: 'Share', title: 'Share'}
+  ]
+});
+
+const csvWriter2 = createCsvWriter({
+  path: 'new_data_filtered.csv',
+  header: [
+      {id: 'Category', title: 'Category'},
+      {id: 'Sales', title: 'Sales'},
+      {id: 'Share', title: 'Share'}
   ]
 });
 
@@ -14,31 +26,21 @@ fs.createReadStream('data.csv')
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    const filtered_results = results.filter(result => result.Sales > 500);
-    const mapped_results = results.map(result => result.Sales);
+    const totalSales = results.reduce((total, value) => total + parseInt(value.Sales),0);
+    const salesShare = results.map(result => ((parseInt(result.Sales)/totalSales)*100));
+    for(let i = 0; i < results.length; i++) {
+      results[i]['Share'] = salesShare[i].toFixed(2)+"%";
+    }
+    console.log(results);
 
+    csvWriter.writeRecords(results)
+    .then(() => {
+        console.log('...Done');
+    });
+
+    csvWriter2.writeRecords(results)
+    .then(() => {
+        console.log('...Done');
+    });
 
   });
-
-
-// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
-// const filtered_results = results.filter(result => result.Sales > 500);
-
-const csvWriter = createCsvWriter({
-    path: 'new_data.csv',
-    header: [
-        {id: 'category', title: 'Category'},
-        {id: 'sales', title: 'Sales'}
-    ]
-});
-
-// const records = [];
-// records.push(filtered_results);
-// console.log(results);
-
-// csvWriter.writeRecords(records)
-//     .then(() => {
-//         console.log('...Done');
-//     });
-
